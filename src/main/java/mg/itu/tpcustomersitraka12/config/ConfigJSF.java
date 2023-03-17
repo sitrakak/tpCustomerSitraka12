aae406040e6c533a3881c64d8f69d0d907840948
sitrakak <shajaniri@gmail.com>
2023-03-17 22:14:28 +0300

    Configuration pour JSF

---
+++ src/main/java/mg/itu/tpcustomersitraka12/config/ConfigJSF.java
@@ -0,0 +1,9 @@
+package mg.itu.tpcustomersitraka12.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
