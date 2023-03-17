4f86874bc406aec1b2f4338e6d1821347cbcf1a6
sitrakak <shajaniri@gmail.com>
2023-03-17 21:54:09 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/itu/tpcustomersitraka12/ejb/CustomerManager.java
@@ -0,0 +1,31 @@
+package mg.itu.tpcustomersitraka12.ejb;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import mg.itu.tpcustomersitraka12.entities.Customer;
+
+/**
+ * Gère la persistance des Customers.
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
